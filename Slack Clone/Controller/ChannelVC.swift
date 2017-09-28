cf715ba55f6cd82298adf4cd80793f446892ddae
Jason Mundie <jasonmundie@Jasons-MacBook-Pro.local>
2017-09-28 10:22:44 -0500

    SWReveal v.2

--- Slack Clone/Controller/ChannelVC.swift
+++ Slack Clone/Controller/ChannelVC.swift
@@ -11,25 +11,11 @@ import UIKit
 class ChannelVC: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+/Users/jasonmundie/Desktop/App Resources/Udemy swift/Dev Slopes/Slack Clone/Slack Clone/Slack Clone/Supporting Files/SWRevealViewController.m        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
