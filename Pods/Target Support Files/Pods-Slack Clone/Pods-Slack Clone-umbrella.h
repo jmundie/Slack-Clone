bbbf8a0f46107eaa29f520892e3e7838dc6a005d
Jason Mundie <jasonmundie@Jasons-MacBook-Pro.local>
2017-09-28 09:38:24 -0500

    Setup

---
+++ Pods/Target Support Files/Pods-Slack Clone/Pods-Slack Clone-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Slack_CloneVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Slack_CloneVersionString[];
+
