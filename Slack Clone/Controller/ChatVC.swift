40969cdfd2f4d8801cce54ed1a44954bec5e8a5c
Jason Mundie <jasonmundie@Jasons-MacBook-Pro.local>
2017-09-28 10:15:46 -0500

    SWReveal

---
+++ Slack Clone/Controller/ChatVC.swift
@@ -0,0 +1,25 @@
+//
+//  ChatVC.swift
+//  Slack Clone
+//
+//  Created by Jason Mundie on 9/28/17.
+//  Copyright © 2017 Jason Mundie. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+//    outlets
+    
+    @IBOutlet weak var menuButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuButton.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+    }
+
+   
+}
